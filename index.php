f063f55ce6fda3c24e1b8669e2ac9bddac970e34
Brandon Burgess <burgess.brandon@student.greenriver.edu>
2023-01-24 17:58:01 -0500

    Added orderForm1.hmtl and changes to index.php

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 //controller
 
+
+
 //turn of error reporting
 ini_set('display_errors',1);
 error_reporting(E_ALL);
+
+//Start session
+session_start();
 
 //require autoload file
 require_once('vendor/autoload.php');
@@ -33,6 +38,21 @@ $f3->route('GET /lunch', function () {
     //instantiate a view
     $view = new Template();
     echo $view->render("views/lunch.html");
+});
+
+//define order form route
+$f3->route('GET /order', function($f3) {
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $_SESSION['food'] = $_POST['food'];
+        $_SESSION['meal'] = $_POST['meal'];
+
+        $f3->reroute('summary');
+    }
+
+    //instantiate a view
+    $view = new Template();
+    echo $view->render("views/orderForm1.html");
 });
 
 //define a lunch route & page. Add image to both
